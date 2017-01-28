22639a9a74564d7056737970f7027b7ba299db05
VentureSrl <riveraleon.pe@gmail.com>
2017-01-28 18:12:30 -0500

    Add files via upload

---
+++ app/src/main/java/ventureapp/register.java
@@ -0,0 +1,38 @@
+package ventureapp.registro;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class register extends AppCompatActivity {
+    TextView usuario;
+    EditText tdni;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        usuario=(TextView) findViewById(R.id.idusario);
+        tdni=(EditText)findViewById(R.id.iddni2);
+    }
+    public void consulta(View v) {
+        DataBaseRegister admin = new DataBaseRegister(this,
+                "Registro", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String dni = tdni.getText().toString();
+        Cursor fila = bd.rawQuery(
+                "select nombre,apellido,edad  from register where dni=" + dni, null);
+        if (fila.moveToFirst()) {
+            usuario.setText("\n"+"Nombre:" +fila.getString(0)+"\n\n"+"Apellido: "+fila.getString(1)+"\n\n"+"Edad: "+fila.getString(2)+" años");
+        } else
+            Toast.makeText(this, "No existe una persona con dicho dni",
+                    Toast.LENGTH_SHORT).show();
+        bd.close();
+
+    }
+}
