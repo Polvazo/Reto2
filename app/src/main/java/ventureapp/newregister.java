acca65a9eb266cf50a34ee731dda3bb3505b1a77
VentureSrl <riveraleon.pe@gmail.com>
2017-01-28 18:30:33 -0500

    Add files via upload

---
+++ app/src/main/java/ventureapp/newregister.java
@@ -0,0 +1,59 @@
+package ventureapp.registro;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class newregister extends AppCompatActivity  {
+    EditText etnombre;
+    EditText etapellido;
+    EditText etedad;
+    EditText etdni;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newregister);
+
+        etnombre=(EditText)findViewById(R.id.idnombre);
+        etapellido=(EditText)findViewById(R.id.idapellido);
+        etedad=(EditText)findViewById(R.id.idedad);
+        etdni=(EditText)findViewById(R.id.iddni);
+    }
+
+
+    public void alta (View v) {
+        DataBaseRegister admin = new DataBaseRegister(this,"Registro",null,1);
+        SQLiteDatabase bd=admin.getWritableDatabase();
+
+        String nombre = etnombre.getText().toString();
+        String apellido= etapellido.getText().toString();
+        String edad = etedad.getText().toString();
+        String dni=etdni.getText().toString();
+
+        ContentValues registro = new ContentValues();
+
+        registro.put("dni",dni);
+        registro.put("nombre",nombre);
+        registro.put("apellido",apellido);
+        registro.put("edad",edad);
+
+        bd.insert("register",null,registro);
+
+        etdni.setText("");
+        etnombre.setText("");
+        etapellido.setText("");
+        etedad.setText("");
+
+        Toast.makeText(this, "Se registro correctamente",
+                Toast.LENGTH_SHORT).show();
+
+    }
+}
